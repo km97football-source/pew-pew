009ade3a609d49525a1cd208e2139462ab6e4b13
Vinee <ru822874@wellingtoncdsb.ca>
2026-03-31 13:34:20 -0400

    synced intro game screen

---
+++ Classes/GameScreen.js
@@ -0,0 +1,65 @@
+
+
+class GameScreen {
+  constructor() {
+    this.state = "menu"; // "menu" or "game"
+
+    this.buttonW = 220;
+    this.buttonH = 70;
+
+    this.bx = width / 2;
+    this.by = height / 2 + 60;
+  }
+
+  update() {
+    // nothing needed yet
+  }
+
+  display() {
+    if (this.state === "menu") {
+      // MENU SCREEN
+      if (bgImg) {
+        image(bgImg, 0, 0, width, height);
+      }
+
+      textAlign(CENTER, CENTER);
+
+      fill(255);
+      textSize(60);
+      text("Jungle Adventure", width / 2, height / 2 - 100);
+
+      // hover effect
+      if (this.isHovering(this.bx, this.by)) {
+        fill(255, 200, 0);
+      } else {
+        fill(255, 150, 0);
+      }
+
+      rectMode(CENTER);
+      rect(this.bx, this.by, this.buttonW, this.buttonH, 12);
+
+      fill(0);
+      textSize(24);
+      text("START", this.bx, this.by);
+
+    } else if (this.state === "game") {
+      // BLANK SCREEN AFTER CLICK
+      background(0);
+    }
+  }
+
+  handleMousePressed() {
+    if (this.state === "menu" && this.isHovering(this.bx, this.by)) {
+      this.state = "game"; // switch screen
+    }
+  }
+
+  isHovering(x, y) {
+    return (
+      mouseX > x - this.buttonW / 2 &&
+      mouseX < x + this.buttonW / 2 &&
+      mouseY > y - this.buttonH / 2 &&
+      mouseY < y + this.buttonH / 2
+    );
+  }
+}
--- Sketch.js
+++ Sketch.js
@@ -1,8 +1,9 @@
+let game;
+let bgImg, img;
 let ninja1, arrow;
 let monsters = [];
 let boss;
 let shurikens = [];
-let img;
 let lastShurikenTime = 0;
 let shurikenDelay = 250;
 
@@ -11,15 +12,22 @@ function preload() {
         () => console.log('Shuriken image loaded'),
         () => console.error('Failed to load shuriken image')
     );
+    bgImg = loadImage('assests/Welcome To Jungle Adventure.png',
+        () => console.log('Background image loaded'),
+        () => console.error('Failed to load background image')
+    );
 }
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background('white');
+	game = new GameScreen();
+}
 
+function initGame() {
 	ninja1 = new Ninja(width / 2, height / 2, 20, 'green', 3)
 	arrow = new Arrow(ninja1, 50);
 
+	monsters = [];
 	monsters.push(new Monster(width / 4, height / 4));
 	monsters.push(new Monster(3 * width / 4, height / 4));
 	monsters.push(new Monster(width / 2, height / 4 + 100));
@@ -28,55 +36,67 @@ function setup() {
 }
 
 function draw() {
-	background('white');
-	ninja1.draw()
-	ninja1.update()
-	arrow.draw()
-	
-	// Update and draw boss
-	if (boss.alive) {
-		boss.update(ninja1.x, ninja1.y);
-		boss.draw();
-	}
+	if (game.state === "menu") {
+		game.update();
+		game.display();
+	} else if (game.state === "game") {
+		background('white');
+		ninja1.draw()
+		ninja1.update()
+		arrow.draw()
+		
+		// Update and draw boss
+		if (boss.alive) {
+			boss.update(ninja1.x, ninja1.y);
+			boss.draw();
+		}
 
-	// Update and draw all monsters
-	for (let m of monsters) {
-		m.update(ninja1.x, ninja1.y);
-		m.draw();
-	}
+		// Update and draw all monsters
+		for (let m of monsters) {
+			m.update(ninja1.x, ninja1.y);
+			m.draw();
+		}
     
-	// Update and draw all shurikens with collision detection
-    for (let i = shurikens.length - 1; i >= 0; i--) {
-        let s = shurikens[i];
-        s.update();
-        s.draw();
-        
-        // Check collision with boss
-        if (boss.alive && dist(s.x, s.y, boss.x, boss.y) < s.size / 2 + boss.size / 2) {
-            boss.takeDamage(10);
-            shurikens.splice(i, 1);
-            print("Boss hit! Health:", boss.health);
-        } else {
-            // Check collision with all monsters
-            for (let j = monsters.length - 1; j >= 0; j--) {
-                let m = monsters[j];
-                if (m.alive && dist(s.x, s.y, m.x, m.y) < s.size / 2 + m.size / 2) {
-                    m.takeDamage(10);
-                    shurikens.splice(i, 1);
-                    print("Monster hit! Health:", m.health);
-                    break;
+		// Update and draw all shurikens with collision detection
+        for (let i = shurikens.length - 1; i >= 0; i--) {
+            let s = shurikens[i];
+            s.update();
+            s.draw();
+            
+            // Check collision with boss
+            if (boss.alive && dist(s.x, s.y, boss.x, boss.y) < s.size / 2 + boss.size / 2) {
+                boss.takeDamage(10);
+                shurikens.splice(i, 1);
+                print("Boss hit! Health:", boss.health);
+            } else {
+                // Check collision with all monsters
+                for (let j = monsters.length - 1; j >= 0; j--) {
+                    let m = monsters[j];
+                    if (m.alive && dist(s.x, s.y, m.x, m.y) < s.size / 2 + m.size / 2) {
+                        m.takeDamage(10);
+                        shurikens.splice(i, 1);
+                        print("Monster hit! Health:", m.health);
+                        break;
+                    }
                 }
             }
         }
-    }
+	}
 }
 
 function mousePressed() {
-    let currentTime = millis();
-    if (currentTime - lastShurikenTime > shurikenDelay) {
-        let newShuriken = new Shuriken(ninja1.x, ninja1.y, 5, mouseX, mouseY);
-        shurikens.push(newShuriken);
-        lastShurikenTime = currentTime;
-        print("shuriken thrown");
-    }
+	if (game.state === "menu") {
+		game.handleMousePressed();
+		if (game.state === "game") {
+			initGame();
+		}
+	} else if (game.state === "game") {
+        let currentTime = millis();
+        if (currentTime - lastShurikenTime > shurikenDelay) {
+            let newShuriken = new Shuriken(ninja1.x, ninja1.y, 5, mouseX, mouseY);
+            shurikens.push(newShuriken);
+            lastShurikenTime = currentTime;
+            print("shuriken thrown");
+        }
+	}
 }
